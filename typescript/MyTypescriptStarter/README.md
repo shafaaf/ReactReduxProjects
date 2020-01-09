8b8e22dfca4afa0ce4dbf7ad5e53ea4af3cefa5e
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2020-01-09 02:08:12 -0500

    ASda

--- typescript/MyTypescriptStarter/README.md
+++ typescript/MyTypescriptStarter/README.md
@@ -4,3 +4,6 @@
 npm install 
 npm run build
 ```
+
+
+Good practises: https://codeburst.io/five-tips-i-wish-i-knew-when-i-started-with-typescript-c9e8609029db
--- typescript/MyTypescriptStarter/tests/index.test.js
+++
@@ -1,7 +0,0 @@
-var dist = require("../dist");
-describe("index", function () {
-    it("should say 'hello world'", function () {
-        dist.SmokeTest.HelloWorld();
-    });
-});
-//# sourceMappingURL=index.test.js.map
