46c1a7c4dca9993ec41fc5691a8397dfa683c59c
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2020-01-12 22:32:09 -0500

    bundling working with normal js

---
+++ typescript/Browserify/main.js
@@ -0,0 +1,10 @@
+console.log("Debug start");
+
+var x = require('arithmetic');
+console.log(x.add(1, 2));
+
+var y = require('chessboardjs');
+y('board', 'start');
+//let board1 = ChessBoard('myBoard', 'start');
+
+console.log("Debug end");
