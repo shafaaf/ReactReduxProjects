ce8e30b96986c90457235eb0d32bd91177a12aab
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2020-01-18 02:53:59 -0500

    removed browserify

--- typescript/Browserify/js/main.js
+++
@@ -1,9 +0,0 @@
-console.log("Debug start");
-
-let x = require('arithmetic');
-console.log(x.add(1, 2));
-
-let chessboard = require('chessboardjs');
-chessboard('board', 'start');
-
-console.log("Debug end");
