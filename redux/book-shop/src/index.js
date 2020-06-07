5910d3b9d649ccfd23d318f8593fe6fa66f91309
Shafaaf <shafaaf.hossain@gmail.com>
2020-06-06 23:09:03 -0400

    sdfdsFDS

--- redux/book-shop/src/index.js
+++ redux/book-shop/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Routes from './routes';
+
 
 import { BrowserRouter } from 'react-router-dom'
 
@@ -9,7 +11,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Routes />
   </BrowserRouter>,
   document.getElementById('root')
 );
---
+++ redux/book-shop/src/routes.js
@@ -0,0 +1,14 @@
+import React  from 'react';
+
+import Home from './components/common/HomePage'
+import About from './components/common/AboutPage'
+import Book from './components/book/BookPage'
+
+const Routes = () => (
+    <div>
+        <h1>asd</h1>
+    </div>
+);
+
+
+export default Routes;
