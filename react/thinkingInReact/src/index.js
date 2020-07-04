ddbffb71e3a68fef26c342071c80cb6152c0ddb3
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2020-07-04 03:04:19 -0400

    sdfs

--- react/thinkingInReact/src/index.js
+++ react/thinkingInReact/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {FilterableProductTable, PRODUCTS} from './components/filterableProductTable';
+import {FilterableProductTable, PRODUCTS} from './components/FilterableProductTable';
 
 
 ReactDOM.render(<FilterableProductTable products={PRODUCTS}/>, document.getElementById('root'));
