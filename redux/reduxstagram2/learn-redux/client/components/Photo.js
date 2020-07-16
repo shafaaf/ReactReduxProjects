5b7a804cf569985e392b9a1fbe20fc94fa0ccde0
Shafaaf <shafaaf.hossain@gmail.com>
2020-07-15 20:19:48 -0400

    sdfs

--- redux/reduxstagram2/learn-redux/client/components/Photo.js
+++ redux/reduxstagram2/learn-redux/client/components/Photo.js
@@ -4,7 +4,8 @@ import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Photo = React.createClass({
     render() {
-        const { post, i, comments } = this.props;
+        const { post, index, comments } = this.props;
+        console.log('index is: ', index);
         return (
             <figure className="grid-figure">
                 <div className="grid-photo-wrap">
@@ -19,15 +20,14 @@ const Photo = React.createClass({
                     <figcaption>
                         <p>{post.caption}</p>
                         <div className="control-buttons">
-                            <button className="likes">&hearts; {post.likes}</button>
-                              <Link className="button" to={`/view/${post.code}`}>
-                            <span className="comment-count">
-                            <span className="speech-bubble"></span>
-                            {comments[post.code] ? comments[post.code].length : 0 }
-                            </span>
+                            <button onClick={this.props.increment.bind(null, index)} className="likes">&hearts; {post.likes}</button>
+                            <Link className="button" to={`/view/${post.code}`}>
+                                <span className="comment-count">
+                                    <span className="speech-bubble"/>
+                                    {comments[post.code] ? comments[post.code].length : 0 }
+                                </span>
                             </Link>
                         </div>
-
                     </figcaption>
 
                 </div>
