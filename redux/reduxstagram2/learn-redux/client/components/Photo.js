0c225b75a9fd4c464197ac267cd2cf7d8caf3b06
Shafaaf <shafaaf.hossain@gmail.com>
2020-07-15 22:28:34 -0400

    did till 18

--- redux/reduxstagram2/learn-redux/client/components/Photo.js
+++ redux/reduxstagram2/learn-redux/client/components/Photo.js
@@ -4,8 +4,7 @@ import CSSTransitionGroup from 'react-addons-css-transition-group';
 
 const Photo = React.createClass({
     render() {
-        const { post, index, comments } = this.props;
-        console.log('index is: ', index);
+        const { post, i, comments } = this.props;
         return (
             <figure className="grid-figure">
                 <div className="grid-photo-wrap">
@@ -20,7 +19,7 @@ const Photo = React.createClass({
                     <figcaption>
                         <p>{post.caption}</p>
                         <div className="control-buttons">
-                            <button onClick={this.props.increment.bind(null, index)} className="likes">&hearts; {post.likes}</button>
+                            <button onClick={this.props.increment.bind(null, i)} className="likes">&hearts; {post.likes}</button>
                             <Link className="button" to={`/view/${post.code}`}>
                                 <span className="comment-count">
                                     <span className="speech-bubble"/>
--- redux/reduxstagram2/learn-redux/client/reducers/posts.js
+++ redux/reduxstagram2/learn-redux/client/reducers/posts.js
@@ -1,5 +1,16 @@
 export default function posts(state = [], action) {
     console.log('posts: state is: ', state);
     console.log('posts: action is: ', action);
-    return state;
+
+    switch (action.type) {
+        case 'INCREMENT_LIKES':
+            const i = action.index;
+            return [
+                ...state.slice(0, i),
+                {...state[i], likes: state[i].likes + 1},
+                ...state.slice(i + 1)
+            ];
+        default:
+            return state;
+    }
 }
