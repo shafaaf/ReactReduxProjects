8bec4e812e8f96ffe69eb66afdf444beb63ce2e7
Shafaaf <shafaaf.hossain@gmail.com>
2020-08-11 23:20:22 -0400

    asdadsa

--- redux/book-shop/src/components/book/Book.js
+++ redux/book-shop/src/components/book/Book.js
@@ -8,15 +8,30 @@ class Book extends React.Component{
     }
 
     submitBook(input){
-        alert('Submitted')
+        this.props.createBook(input);
     }
 
-    render(){
-        // Title input tracker
+    render() {
         let titleInput;
-        // return JSX
-        return(
+        return (
+            <div style={{textAlign: "center"}}>
             <h1>Books Page</h1>
+                <ul>
+                    {this.props.books.map((b, i) => <li key={i}>{b.title}</li> )}
+                </ul>
+                <div>
+                    <h3>Books Form</h3>
+                    <form onSubmit={e => {
+                        e.preventDefault();
+                        const input = {title: titleInput.value};
+                        this.submitBook(input);
+                        e.target.reset();
+                    }}>
+                        <input type="text" name="title" ref={node => titleInput = node}/>
+                        <input type="submit" />
+                    </form>
+                </div>
+            </div>
         )
     }
 }
