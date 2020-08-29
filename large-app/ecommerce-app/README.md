6e481e68996c4459533b532de1ac6c71798f37e6
Shafaaf <shafaaf.hossain@gmail.com>
2020-08-29 01:21:29 -0400

    Sdfsd

--- large-app/ecommerce-app/README.md
+++ large-app/ecommerce-app/README.md
@@ -3,3 +3,6 @@
 Tutorial: https://www.youtube.com/watch?v=_pkBqnyxY0w&list=PLeh2GWv22bmQHOAYllM8ycm3obDexyoLg
 Demo: https://react-shopping-cart-final.herokuapp.com/
 Code: https://github.com/basir/react-shopping-cart
+
+## TODO
+Part 05 Products List Component complete
--- large-app/ecommerce-app/src/App.js
+++ large-app/ecommerce-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import data from './data';
 import Products from "./ components/Products";
+import Filter from "./ components/Filter";
 
 class App extends React.Component {
     constructor() {
@@ -13,6 +14,31 @@ class App extends React.Component {
         };
         console.log("products is: ", this.state.products);
     }
+
+    sortProducts = e => {
+        const sortBy = e.target.value;
+        console.log("calling sortProducts: ", sortBy);
+    };
+
+    filterProductsBySize = e => { // filter by size
+        const size = e.target.value;
+        console.log("calling filterProducts: ", size);
+
+        if (size === "ALL") { // show all products
+            console.log("All selected");
+            this.setState({
+                products: data.products
+            });
+        } else { // filter by some size
+            this.setState({
+                size: size,
+                products: data.products.filter(product =>
+                    product.availableSizes.indexOf(size) >= 0
+                )
+            });
+        }
+    };
+
     render() {
         return (
             <div className="App">
@@ -23,6 +49,13 @@ class App extends React.Component {
                     <main>
                         <div className="content">
                             <div className="main">
+                                <Filter
+                                    count = {this.state.products.length}
+                                    size = {this.state.size}
+                                    sort = {this.state.sort}
+                                    filterProductsBySize = {this.filterProductsBySize}
+                                    sortProducts = {this.sortProducts}
+                                />
                                 <Products products = {this.state.products}/>
                             </div>
                             <div className="sidebar">
