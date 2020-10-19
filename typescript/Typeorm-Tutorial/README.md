0895b7460467fbd17f8e1763ca60b46f277c365b
shafaaf <shafaah@amazon.com>
2020-10-19 02:50:48 -0400

    asDA

---
+++ typescript/Typeorm-Tutorial/README.md
@@ -0,0 +1,11 @@
+# Awesome Project Build with TypeORM
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `ormconfig.json` file
+3. Run `npm start` command
+
+Tutorial from:
+https://www.youtube.com/watch?v=Reb7ISQZCvA&t=1934s&ab_channel=CoderOne
+https://ipenywis.com/learn-typeorm-on-node-js-with-mysql-from-scratch-in-one-video-42126f6fd3bf
---
+++ typescript/Typeorm-Tutorial/src/entity/User.ts
@@ -0,0 +1,20 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Tweet } from './Tweet';
+
+@Entity({ name: "users" })
+export class User {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column()
+    age: number;
+
+    @OneToMany((type) => Tweet, (tweet) => tweet.user)
+    tweets: Tweet[];
+}
