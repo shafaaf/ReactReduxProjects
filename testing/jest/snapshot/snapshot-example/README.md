360f8edf7fce6b2d95d9c60cad970a491fa89706
shafaaf <shafaah@amazon.com>
2020-10-31 18:25:00 -0400

    dfs

--- testing/jest/snapshot/snapshot-example/README.md
+++ testing/jest/snapshot/snapshot-example/README.md
@@ -1,2 +1,3 @@
 ## React Snapshot example
 
+https://www.digitalocean.com/community/tutorials/how-to-write-snapshot-tests-for-react-components-with-jest
---
+++ testing/jest/snapshot/snapshot-example/src/components/Items.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Render a list of items
+ *
+ * @param {Object} props - List of items
+ */
+class Items extends React.Component {
+    render() {
+        const {items = []} = this.props;
+
+        // A single item in the list, render a span.
+        if (items.length === 1) {
+            return <span>{items[0]}</span>;
+        }
+
+        // Multiple items on the list, render a list.
+        if (items.length > 1) {
+            return (
+                <ul>
+                    {items.map(item => <li key={item}>{item}</li>)}
+                </ul>
+            );
+        }
+
+        // No items on the list, render an empty message.
+        return <span>No items in list</span>;
+    }
+}
+
+Items.propTypes = {
+    items: PropTypes.array,
+};
+
+Items.defaultProps = {
+    items: []
+};
+
+export default Items;
