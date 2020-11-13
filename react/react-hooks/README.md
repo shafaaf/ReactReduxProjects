73f279bdd825622b7f7cb731da7667a7562db57c
shafaah <shafaah@amazon.com>
2020-11-12 22:10:16 -0500

    Asda

---
+++ react/react-hooks/README.md
@@ -0,0 +1,5 @@
+# React hooks tutorial
+
+Tutorial: https://medium.com/@dtkatz/react-hooks-tutorial-learn-by-building-b90ec4db2b8e
+Demo: https://davidtkatz.com/#/typerace
+Code: https://github.com/15Dkatz/typerace-hooks
---
+++ react/react-hooks/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+    const SNIPPETS = [
+        'Bears, beets, battlestar galactica',
+        "What's Forrest Gump's password? 1Forrest1",
+        'Where do programmers like to hangout? The Foo Bar'
+    ];
+
+    const INITIAL_GAME_STATE = { victory: false, startTime: null, endTime: null };
+    const [snippet, setSnippet] = useState('');
+    const [userText, setUserText] = useState('');
+    const [gameState, setGameState] = useState(INITIAL_GAME_STATE);
+
+    useEffect(() => {
+        if (gameState.victory) document.title = 'Victory!';
+    });
+
+    const chooseSnippet = snippetIndex => () => {
+        setSnippet(SNIPPETS[snippetIndex]);
+        setGameState({ ...gameState, victory: false, startTime: new Date().getTime(), endTime: null});
+        setUserText('');
+    };
+
+    const updateUserText = event => {
+        setUserText(event.target.value);
+
+        if (event.target.value === snippet) {
+            setGameState({
+                ...gameState,
+                victory: true,
+                endTime: new Date().getTime()
+            });
+        }
+
+    };
+
+    const millisToMinutesAndSeconds = (startTime, endTime) => {
+        const millis = endTime - startTime;
+        const minutes = Math.floor(millis / 60000);
+        const seconds = ((millis % 60000) / 1000).toFixed(0);
+        return minutes + "mins, " + (seconds < 10 ? '0' : '') + seconds;
+    };
+
+    return (
+        <div>
+            <h2>Type Race</h2>
+            <hr />
+            <h3>Snippet</h3>
+            {snippet}
+            <h4>{gameState.victory ? `Done! 🎉 Time: ${millisToMinutesAndSeconds(gameState.startTime, gameState.endTime)}s` : null}</h4>
+
+            <input value={userText} onChange={updateUserText} />
+            <hr />
+            {
+                SNIPPETS.map((SNIPPET, index) => (
+                    <button onClick={chooseSnippet(index)} key={index}>
+                        {SNIPPET.substring(0, 10)}...
+                    </button>
+                ))
+            }
+        </div>
+    )
+};
+
+export default App;
