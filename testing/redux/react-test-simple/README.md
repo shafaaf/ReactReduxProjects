e9769fb36e68786a73d811871f3ee7026277551c
shafaah <shafaah@amazon.com>
2020-11-19 23:15:53 -0500

    dass

---
+++ testing/redux/react-test-simple/README.md
@@ -0,0 +1,13 @@
+# React-Test-Simple
+
+> A minimal demonstration of how to get started with testing react redux apps
+
+``` bash
+# install dependencies
+npm install
+
+# run all tests
+npm test
+```
+
+Article: https://scotch.io/tutorials/testing-react-and-redux-apps-with-jest
---
+++ testing/redux/react-test-simple/src/redux/actions.js
@@ -0,0 +1,11 @@
+export const editTodo = () => {
+  console.log('Sample editTodo async function');
+};
+
+export const toggleTodo = () => {
+  console.log('Sample toggleTodo async function');
+};
+
+export const deleteTodo = () => {
+  console.log('Sample deleteTodo async function');
+};
