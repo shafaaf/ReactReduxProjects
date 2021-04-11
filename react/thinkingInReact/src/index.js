25d2b6d30284c3a8f638a889068ce9be3b991b14
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2021-04-11 16:18:28 -0400

    asdas

--- react/thinkingInReact/src/index.js
+++ react/thinkingInReact/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {FilterableProductTable, PRODUCTS} from './components/FilterableProductTable';
 
+import {FilterableProductTable} from './components/FilterableProductTable';
+import {PRODUCTS} from './data'
 
 ReactDOM.render(<FilterableProductTable products={PRODUCTS}/>, document.getElementById('root'));
