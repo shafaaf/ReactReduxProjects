4b036bdf6b2ac6d85dd7fd4a7de618cdc6ba61c5
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2021-04-14 03:57:15 -0400

    adeas

--- node/crud-app-1/config/db.config.js
+++ node/crud-app-1/config/db.config.js
@@ -2,7 +2,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const username = process.env.MONGODB_USERNAME;
 const password = process.env.MONGODB_PASSSWORD;
+const clusterUrl = process.env.CLUSTER_URL;
 
 module.exports = {
-    url: `mongodb+srv://${username}:${password}@cluster0.fw7uc.mongodb.net/myFirstDatabase`
+    url: `mongodb+srv://${username}:${password}@${clusterUrl}/myFirstDatabase`
 };
