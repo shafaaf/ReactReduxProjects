e1d314d499f975f5e9da90a1c992054583e93e3e
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2021-04-21 23:33:12 -0400

    adsa

--- large-app/netflix-clone-2/README.md
+++ large-app/netflix-clone-2/README.md
@@ -4,6 +4,6 @@ Uses Tmdb api
 Tutorial from: https://www.youtube.com/watch?v=XtMThy8QKqU&t=2433s&ab_channel=CleverProgrammer
 Learning React + CSS
 
-Did till: 1:52:00 - creating banner
+Did till: 1:54:00 - creating banner
 
 CSS sections for rows explained from 01:10:00
--- large-app/netflix-clone-2/src/components/Banner.js
+++ large-app/netflix-clone-2/src/components/Banner.js
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react';
 import axiosInstance from "../axios";
 import requests from "../requests";
 
+import './Banner.css';
+
 function Banner() {
     const [movie, setMovie] = useState([]);
 
@@ -12,8 +14,17 @@ function Banner() {
             setMovie (request.data.results[randomIndex]);
             return request;
         }
-        fetchData().then(() => console.log(movie));
+        console.log("in useEffect: movie" + movie);
+        fetchData().then(() => {
+            console.log("in useEffect after fetchData: movie" + movie)
+        });
     }, []);
+
+    console.log("after useEffect: movie" + movie);
+
+    const truncate = (str, n) =>
+        str?.length > n ? str.substr(0, n - 1) + "..." : str;
+
 
     return (
         <div>
@@ -21,12 +32,21 @@ function Banner() {
                 className="banner"
                 style={{
                     backgroundSize: "cover",
-                    backgroundImage: `url(https://www.irmi.com/images/default-source/article-images/aviation/boeing-737.jpg?sfvrsn=4)`,
+                    backgroundImage: `url(https://wallpaperaccess.com/full/493220.jpg)`,
                     backgroundPosition: "center center"
-                }}
-
-            >
+                }}>
                 <div className="banner_contents">
+                    <h1 className = "banner_title">
+                        {movie?.title || movie?.name || movie?.original_name}
+                    </h1>
+                    <div className="banner_buttons">
+                        <button className="banner_button">Play</button>
+                        <button className="banner_button">My List</button>
+                    </div>
+
+                    <h1 className="banner_description">
+                        {truncate(movie?.overview, 150)}
+                    </h1>
 
                 </div>
             </header>
