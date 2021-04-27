55b355c4ea067151a556f5ebea62b245363c8192
Shafaaf Hossain <EL-CAPITAN-2016@Mackintoshs-MacBook-Pro-3.local>
2021-04-27 01:09:24 -0400

    asdsa

--- large-app/netflix-clone-2/src/App.js
+++ large-app/netflix-clone-2/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Row from "./components/Row";
 import requests from "./requests";
 import Banner from "./components/Banner";
+import Nav from "./components/Nav";
 
 function App() {
     return (
         <div className="App">
+            <Nav/>
             <Banner/>
             <Row title = "Netflix Originals" fetchUrl = {requests.fetchNetflixOriginals} isLargeRow = {true}/>
             <Row title = "Trending Now" fetchUrl = {requests.fetchTrending}/>
---
+++ large-app/netflix-clone-2/src/components/Nav.js
@@ -0,0 +1,20 @@
+import './Nav.css';
+
+export default function Nav() {
+    return (
+        <div className="nav">
+            <img
+                className="nav_logo"
+                src="https://cdn.vox-cdn.com/thumbor/QuS2QKQys3HhosKiV-2IuKhphbo=/39x0:3111x2048/1400x1050/filters:focal(39x0:3111x2048):format(png)/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png"
+                alt="Netflix Logo"
+            />
+
+            <img
+                className="nav_avatar"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+                alt="Netflix Avatar"
+            />
+
+        </div>
+    )
+}
